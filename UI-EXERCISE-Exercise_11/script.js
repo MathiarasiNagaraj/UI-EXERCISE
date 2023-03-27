9624623b2344a68b1837d613dedbb99b63b90500
MathiarasiNagaraj <125631817+MathiarasiNagaraj@users.noreply.github.com>
2023-03-27 19:12:17 +0530

    Update script.js

--- UI-EXERCISE-Exercise_11/script.js
+++ UI-EXERCISE-Exercise_11/script.js
@@ -1,38 +1,42 @@
-let number1 = prompt("enter a number");
-console.log(number1);
-const calculator = {
+//Calculator object with add,sub,multiply,divide function
+const Calculator = {
     number1:10,
     number2: 20,
-    isValid:(n1,n2)=>typeof(n1)=="number"&&typeof(n2)=="number",
-    Add: (n1, n2) => {
-        if (this.isValid(n1, n2))
+    isValid: function (n1, n2) { 
+        if (typeof n1 !=="number"||typeof n2 !=="number")
+            return false;
+        else
+            return true;
+    },
+    add:function(n1, n2){
+        if (Calculator.isValid(n1, n2))
             return n1 + n2;
         else
-            "Enter a valid number"
+            alert("Enter a valid number");
     },
-    Subtract: (n1, n2) => {
-        if (this.isValid(n1, n2))
+    subtract: function(n1, n2) {
+        if (Calculator.isValid(n1, n2))
             return n1 - n2;
         else
-            "Enter a valid number"
+        alert("Enter a valid number");
     },
-    Multiply: (n1, n2) => {
-        if (this.isValid(n1, n2))
+    multiply:function (n1, n2){
+        if (Calculator.isValid(n1, n2))
             return n1 * n2;
         else
-            "Enter a valid number"
+        alert("Enter a valid number");
     },
-    Divide: (n1, n2) => {
-        if (this.isValid(n1, n2))
+    divide:function(n1, n2) {
+        if (Calculator.isValid(n1, n2))
             if(n2!=0)
                 return n1 / n2;
             else
-                "Enter a valid denominator"
+            alert("Enter a valid denominator");
         else
-            "Enter a valid number"
+        alert("Enter a valid number");
     }
 }
-console.log(calculator.Add(10, 12));
-console.log(calculator.Subtract(calculator.number1, calculator.number2));
-console.log(calculator.Multiply(calculator.number1, calculator.number2));
-console.log(calculator.Divide(calculator.number1, calculator.number2))
+console.log(Calculator.add('10', 12));
+console.log(Calculator.subtract(Calculator.number1, Calculator.number2));
+console.log(Calculator.multiply(Calculator.number1, Calculator.number2));
+console.log(Calculator.divide(Calculator.number1, Calculator.number2))
